6d9739887fed76a6d43f75ba8adc7103fffc1b0a
Rodrigo Centellas <rodrigo.rcd78@gmail.com>
2025-06-06 20:41:23 -0400

    correccion_endPoint_citas

--- src/main/java/com/historiac/service/mapper/CitaMedicaMapper.java
+++ src/main/java/com/historiac/service/mapper/CitaMedicaMapper.java
@@ -13,17 +13,29 @@ import org.mapstruct.*;
  */
 @Mapper(componentModel = "spring")
 public interface CitaMedicaMapper extends EntityMapper<CitaMedicaDTO, CitaMedica> {
-    @Mapping(target = "paciente", source = "paciente", qualifiedByName = "pacienteId")
-    @Mapping(target = "personalMedico", source = "personalMedico", qualifiedByName = "personalMedicoId")
-    CitaMedicaDTO toDto(CitaMedica s);
+    @Mapping(target = "paciente", source = "paciente", qualifiedByName = "mapPacienteFull")
+    @Mapping(target = "personalMedico", source = "personalMedico", qualifiedByName = "mapPersonalMedicoFull")
+    CitaMedicaDTO toDto(CitaMedica citaMedica);
 
-    @Named("pacienteId")
-    @BeanMapping(ignoreByDefault = true)
+    @Named("mapPacienteFull")
     @Mapping(target = "id", source = "id")
-    PacienteDTO toDtoPacienteId(Paciente paciente);
+    @Mapping(target = "nombre", source = "nombre")
+    @Mapping(target = "fechaNacimiento", source = "fechaNacimiento")
+    @Mapping(target = "genero", source = "genero")
+    @Mapping(target = "direccion", source = "direccion")
+    @Mapping(target = "carnetidentidad", source = "carnetidentidad")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "password", ignore = true) // Seguridad
+    @Mapping(target = "telefonoContacto", source = "telefonoContacto")
+    @Mapping(target = "historialMedico", source = "historialMedico")
+    PacienteDTO toDtoPacienteFull(Paciente paciente);
 
-    @Named("personalMedicoId")
-    @BeanMapping(ignoreByDefault = true)
+    @Named("mapPersonalMedicoFull")
     @Mapping(target = "id", source = "id")
-    PersonalMedicoDTO toDtoPersonalMedicoId(PersonalMedico personalMedico);
+    @Mapping(target = "nombre", source = "nombre")
+    @Mapping(target = "especialidad", source = "especialidad")
+    @Mapping(target = "telefonoContacto", source = "telefonoContacto")
+    @Mapping(target = "correo", source = "correo")
+    @Mapping(target = "licenciaMedica", source = "licenciaMedica")
+    PersonalMedicoDTO toDtoPersonalMedicoFull(PersonalMedico personalMedico);
 }
